9d2102826c9c25e7bf2d9105ecf4e41d80150497
Jaksylyk1881 <jaksjuasbai@gmail.com>
2023-03-26 22:11:11 +0600

    Added: lang and packages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return [
+    'email_ignore' => 'If you have not previously taken any action in applications :app_name, ignore this email.',
+    'email_dont_answer' => 'This email is generated automatically, there is no need to respond to it.',
+
+    'object' => '[0,1] object|[2,*] objects',
+];
